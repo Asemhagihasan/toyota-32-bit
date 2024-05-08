ee16accb9d13cb1c2b39e0d80fdd16b212b52679
Asem hagi hasan <asemhagihasan12345@gmail.com>
2024-05-08 19:42:44 +0300

    Implement Logo Component with MUI Box

---
+++ src/ui/Logo.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+
+function Logo() {
+  return (
+    <Box sx={{ width: "200px" }}>
+      <img
+        style={{ width: "200px" }}
+        src="https://camo.githubusercontent.com/e8cb68e1a6662d2727f384fa6478a58f8446f738aba899c2904bbe3e42f745f8/68747470733a2f2f33326269742e636f6d2e74722f77702d636f6e74656e742f75706c6f6164732f323031392f30392f33324269745f5472616e73706172656e7442475f31353030783433342e706e67"
+        alt=""
+      />
+    </Box>
+  );
+}
+
+export default Logo;
