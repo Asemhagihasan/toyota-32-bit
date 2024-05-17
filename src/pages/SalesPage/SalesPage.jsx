32bfedb3a03b530a30ed55d9f4be7effce35ca0f
Asem hagi hasan <asemhagihasan12345@gmail.com>
2024-05-17 16:19:42 +0300

    Update SalesPage layout and add ProductControlPanel

--- src/pages/SalesPage/SalesPage.jsx
+++ src/pages/SalesPage/SalesPage.jsx
@@ -2,21 +2,36 @@ import { Box, Stack } from "@mui/material";
 import LocationSetter from "../../features/sales/LocationSetter";
 import { Outlet } from "react-router-dom";
 import Cart from "../../features/Cart/Cart";
+import ProductControlPanel from "../../features/sales/ProductControlPanel";
 
 function SalesPage() {
   return (
     <>
-      <Box sx={{ position: "relative", height: "100%" }}>
+      <Box
+        sx={{
+          position: "relative",
+          height: "100%",
+          padding: "1rem 1rem ",
+        }}
+      >
         <Stack
           spacing={1}
           sx={{
-            padding: "2rem 1.2rem",
-            maxWidth: "800px",
-            marginLeft: { xs: "2.5rem", lg: "0" },
+            paddingTop: "1.2rem",
+            width: "100%",
+            // marginLeft: { xs: "2.5rem", lg: "0" },
           }}
         >
           <LocationSetter />
-          <Outlet />
+          <Stack
+            direction="row-reverse"
+            flexWrap="wrap"
+            justifyContent="start"
+            gap={2}
+          >
+            <ProductControlPanel />
+            <Outlet />
+          </Stack>
         </Stack>
       </Box>
       <Cart />
