e10a5710d856c2db7b1b9f76692171cc23ac54e1
Asem hagi hasan <asemhagihasan12345@gmail.com>
2024-05-18 22:38:01 +0300

    Integrate ProductPanelProvider into App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import routes from "./routes";
 import { CartProvider } from "./context/CartContext";
+import { ProductPanelProvider } from "./context/ProductControlPanelContext";
 
 function App() {
   const router = createBrowserRouter(routes);
   return (
     <AuthProvider>
-      <CartProvider>
-        <RouterProvider router={router} />
-      </CartProvider>
+      <ProductPanelProvider>
+        <CartProvider>
+          <RouterProvider router={router} />
+        </CartProvider>
+      </ProductPanelProvider>
     </AuthProvider>
   );
 }
