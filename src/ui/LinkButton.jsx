39559d4d8d80d6710c575f18d769e06d537d3fba
Asem hagi hasan <asemhagihasan12345@gmail.com>
2024-06-11 20:42:01 +0300

    Added props parameter to the LinkButton component

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,16 +1,23 @@
 import { Link, useNavigate } from "react-router-dom";
 
-function LinkButton({ children, to }) {
+function LinkButton({ children, to, ...props }) {
   const navigate = useNavigate();
   if (to === "-1") {
     return (
-      <button className="link" to={to} onClick={() => navigate(-1)}>
+      <button
+        className="link"
+        to={to}
+        onClick={() => {
+          navigate(-1);
+        }}
+        {...props}
+      >
         {children}
       </button>
     );
   }
   return (
-    <Link className="link" to={to}>
+    <Link className="link" to={to} {...props}>
       {children}
     </Link>
   );
