ba6eb190aed30dd90e7eb90914bf23c4c34b835f
Asem hagi hasan <asemhagihasan12345@gmail.com>
2024-06-13 15:11:07 +0300

    Refactor ProductControlPanel to integrate dynamic reductions

--- src/features/sales/ProductControlPanel.jsx
+++ src/features/sales/ProductControlPanel.jsx
@@ -3,11 +3,35 @@ import CustomInput from "../../ui/CustomInput";
 import ItemList from "../../ui/ItemList";
 import VirtualKeyboard from "../VirtualKeyboard/Keyboard";
 import { useInputControl } from "../../context/InputControlContext";
+import Reduction from "../../utils/Reduction";
 import { useCart } from "../../context/CartContext";
+
+const reductions = [
+  new Reduction(
+    "10% Discount for Purchases Over 40 $",
+    "10% discount on purchases over 40 $",
+    20,
+    { minPurchase: 40 }
+  ),
+  new Reduction(
+    "10% Wednesday Discount",
+    "10% discount only on Wednesdays",
+    10,
+    {
+      dayOfWeek: 3,
+    }
+  ),
+  new Reduction(
+    "15% Weekend Discount",
+    "15% discount on Saturdays and Sundays",
+    15,
+    { dayOfWeek: 6 }
+  ),
+];
 
 function ProductControlPanel() {
   const { value, onChangeInput, keyboard, setValue } = useInputControl();
-  const { setAppliedPromotion, total } = useCart();
+  const { setReduction } = useCart();
 
   return (
     <Box
@@ -54,21 +78,10 @@ function ProductControlPanel() {
       <ItemList
         sx={{ width: "365px" }}
         text="kampanya getir"
-        items={[
-          {
-            description: "30 $ ve üzeri %10 indirim",
-            disable: total.subTotal < 30,
-            handelClick: () => {
-              setAppliedPromotion("30 $ ve üzeri %10 indirim");
-            },
-          },
-          {
-            description: "3 Al 2 ode",
-            handelClick: () => {
-              setAppliedPromotion("3 Al 2 ode");
-            },
-          },
-        ]}
+        items={reductions}
+        handelClick={(reduction) => {
+          setReduction(reduction);
+        }}
       />
       <Stack sx={{ width: "375px" }}>
         <VirtualKeyboard ip={true} keyboard={keyboard} setInput={setValue} />
