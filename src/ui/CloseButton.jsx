7a9130613559d2a791bc3c44b3b3f47b55cd59af
Asem hagi hasan <asemhagihasan12345@gmail.com>
2024-06-15 17:01:28 +0300

    Path a Refactor CloseButton component to optimize styles and props usage

--- src/ui/CloseButton.jsx
+++ src/ui/CloseButton.jsx
@@ -1,18 +1,14 @@
 import { IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-function CloseButton({ setClicked, sx }) {
+function CloseButton({ sx, ...props }) {
   return (
     <IconButton
-      onClick={() => setClicked(false)}
       sx={{
-        width: "2rem",
-        height: "2rem",
         margin: "0.7rem 0 0.5rem 1.5rem",
-        border: "1px solid var(--color-grey-200)",
         color: "var(--color-grey-700)",
-        boxShadow: "0 3px 5px rgba(0,0,0,.15)",
         ...sx,
       }}
+      {...props}
     >
       <CloseIcon />
     </IconButton>
