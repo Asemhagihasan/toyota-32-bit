46916dc25e53ed5961efd43391eff4972378b0a0
Asem hagi hasan <asemhagihasan12345@gmail.com>
2024-06-15 16:10:03 +0300

    Warp the App component content inside LanguageProvider Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import routes from "./routes";
 import { CartProvider } from "./context/CartContext";
 import { InputControlProvider } from "./context/InputControlContext";
 import { ConnectionStatusProvider } from "./context/ConnectionStatus";
+import { DarkModeProvider } from "./context/DarkModeContext";
+import { LanguageProvider } from "./context/LanguageContext ";
 
 function App() {
   const router = createBrowserRouter(routes);
   return (
-    <AuthProvider>
-      <ConnectionStatusProvider>
-        <InputControlProvider>
-          <CartProvider>
-            <RouterProvider router={router} />
-          </CartProvider>
-        </InputControlProvider>
-      </ConnectionStatusProvider>
-    </AuthProvider>
+    <DarkModeProvider>
+      <AuthProvider>
+        <LanguageProvider>
+          <ConnectionStatusProvider>
+            <InputControlProvider>
+              <CartProvider>
+                <RouterProvider router={router} />
+              </CartProvider>
+            </InputControlProvider>
+          </ConnectionStatusProvider>
+        </LanguageProvider>
+      </AuthProvider>
+    </DarkModeProvider>
   );
 }
 
