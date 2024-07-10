6d8740bcd9eface6d5821eca5fe3d60ae1260199
Asem hagi hasan <asemhagihasan12345@gmail.com>
2024-07-10 21:03:35 +0300

    Added a toaster message

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,46 @@ import { InputControlProvider } from "./context/InputControlContext";
 import { ConnectionStatusProvider } from "./context/ConnectionStatus";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import { LanguageProvider } from "./context/LanguageContext ";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const router = createBrowserRouter(routes);
   return (
-    <AuthProvider>
-      <DarkModeProvider>
-        <LanguageProvider>
-          <ConnectionStatusProvider>
-            <InputControlProvider>
-              <CartProvider>
-                <RouterProvider router={router} />
-              </CartProvider>
-            </InputControlProvider>
-          </ConnectionStatusProvider>
-        </LanguageProvider>
-      </DarkModeProvider>
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <DarkModeProvider>
+          <LanguageProvider>
+            <ConnectionStatusProvider>
+              <InputControlProvider>
+                <CartProvider>
+                  <RouterProvider router={router} />
+                </CartProvider>
+              </InputControlProvider>
+            </ConnectionStatusProvider>
+          </LanguageProvider>
+        </DarkModeProvider>
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-grey-0)",
+              color: "var(--color-grey-700)",
+            },
+          }}
+        />
+      </AuthProvider>
+    </>
   );
 }
 
